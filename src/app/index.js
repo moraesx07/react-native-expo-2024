1d53dcad5565a10b6c44827acb818fa67e044ec9
guilherme <159555227+moraesx07@users.noreply.github.com>
2024-08-20 22:54:25 +0000

    [feat] config do hook e autenticacao

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useAuth } from "../hooks/Auth";
 
 export default function App() {
+  const { signIn, signOut } = useAuth();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Aplicativo Pronto para Usar </Text>
+      <Button
+        title="SignIn Super"
+        onPress={() =>
+          signIn({ email: "super@gmail.com", password: "Super123!" })
+        }
+      />
+      <Button
+        title="SignIn Adm"
+        onPress={() =>
+          signIn({ email: "adm@gmail.com", password: "Adm123!" })
+        }
+      />
+      <Button
+        title="SignIn User"
+        onPress={() =>
+          signIn({ email: "user@gmail.com", password: "User123!" })
+        }
+      />
+      <Button title="SignOut" onPress={() => signOut()} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,12 +34,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontFamily: "regular",
     fontSize: 20,
-  }
+  },
 });
