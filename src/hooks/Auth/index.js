c30f2c21b90258f92bc71f9c7cb176088a3b02c2
guilherme <159555227+moraesx07@users.noreply.github.com>
2024-08-28 15:43:43 +0000

    [feat] Async Storage users

--- src/hooks/Auth/index.js
+++ src/hooks/Auth/index.js
@@ -1,5 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useUsersDatabase } from "../../database/useUsersDatabase";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ActivityIndicator, Text, View } from "react-native";
 
 const AuthContext = createContext({});
 
@@ -18,6 +20,32 @@ export function AuthProvider({ children }) {
 
   const { authUser } = useUsersDatabase();
 
+  useEffect(() => {
+    const loadStorageData = async () => {
+      const storageUser = await AsyncStorage.getItem("@paymentuser");
+
+      if (storageUser) {
+        setUser({
+          autenticated: true,
+          user: JSON.parse(storageUser),
+          role: JSON.parse(storageUser).role,
+        });
+      } else {
+        setUser({
+          autenticated: false,
+          user: null,
+          role: null,
+        });
+      }
+    };
+
+    loadStorageData();
+  }, []);
+
+  useEffect(() => {
+    console.log("AuthProvider", user);
+  }, [user]);
+
   const signIn = async ({ email, password }) => {
     const response = await authUser({ email, password });
     console.log(response);
@@ -29,22 +57,36 @@ export function AuthProvider({ children }) {
         role: null,
       });
       throw new Error("Usuário ou senha inválidos");
-    } else {
-      setUser({
-        autenticated: true,
-        user: response,
-        role: response.role,
-      });
     }
+
+    await AsyncStorage.setItem("@paymentuser", JSON.stringify(response));
+
+    setUser({
+      autenticated: true,
+      user: response,
+      role: response.role,
+    });
   };
 
   const signOut = async () => {
+    await AsyncStorage.removeItem("@paymentuser");
     setUser({});
   };
-
+  
   useEffect(() => {
     console.log("AuthProvider", user);
   }, [user]);
+
+  if (user? autenticated === null) {
+    return ( 
+     <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+       <Text style={{fontSize: 28, marginTop: 15}}>
+       Carregando Dados do Usuário
+       </Text>
+    <ActivityIndicatoryIndicator size="large" color="#0000ff" />
+    </View>
+    );
+   }
 
   return (
     <AuthContext.Provider value={{ user, signIn, signOut }}>
