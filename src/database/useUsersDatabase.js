f41a6f827504d8bfd60b2aefe69493be39cd0daa
guilherme <159555227+moraesx07@users.noreply.github.com>
2024-10-16 15:47:31 +0000

    [feat] metade da aula 14

--- src/database/useUsersDatabase.js
+++ src/database/useUsersDatabase.js
@@ -4,7 +4,7 @@ export function useUsersDatabase() {
   const database = useSQLiteContext();
 
   async function authUser({ email, password }) {
-   // console.log("authUser: ", email, "- password:", password);
+    // console.log("authUser: ", email, "- password:", password);
     try {
       const result = await database.getFirstAsync(`
                 SELECT id, nome, email, role FROM users WHERE email = '${email}' and senha = '${password}'
@@ -16,7 +16,35 @@ export function useUsersDatabase() {
     }
   }
 
+  async function createUser({
+    user_id,
+    user_cadastro,
+    valor_pago,
+    data_pagamento,
+    obervaçao,
+  }) {
+    const statment = await database.prepareAsync(`
+      INSERT INTO payments (user_id, user_cadastro, valor_pago, data_pagamento, obervaçao)
+       VALUES ($user_id, $user_cadastro, $valor_pago, $data_pagamento, $observacao);
+      `);
+    try {
+      const result = await statment.executeAsync({
+        $user_id: user_id,
+        $user_cadastro: user_cadastro,
+        $valor_pago: valor_pago,
+        $data_pagamento: data_pagamento,
+        $observacao: obervaçao,
+      });
+      const insertID = result.lastInsertRowId.toString();
+      return { insertID };
+    } catch (error) {
+      console.log(error)
+    } finally {
+      await statment.finalizeAsync();
+    }
+  }
+
   return {
-    authUser,
+    authUser, createUser
   };
 }
